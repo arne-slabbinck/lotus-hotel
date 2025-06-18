dedc15dff933c13a0f1e661ff4efcefa9a004e21
Arne Slabbinck <arne.slabbinck@gmail.com>
2025-06-18 23:44:16 +0200

    first commit after add

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+
+    <div>
+        <h1 className="highlighted-text-shadow">
+          <p className='under'>The</p>
+          &nbsp; White Lotus
+        </h1>
+    </div>
+
+  );
+}
