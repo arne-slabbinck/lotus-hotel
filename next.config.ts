5dbfd6a59b3f599f8a52b532608db0cd518f1fc8
Arne Slabbinck <arne.slabbinck@gmail.com>
2025-06-18 23:46:27 +0200

    commit removing base path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    basePath: "/lotus-hotel",
+    // basePath: "/lotus-hotel",
     output: "export",
     reactStrictMode: true,
 };
